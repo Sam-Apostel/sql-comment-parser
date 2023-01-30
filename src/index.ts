40b7b855fa24b71278bf424b6a5ee883f4a9cbeb
Sam Apostel <sam@apostel.be>
2023-01-30 12:22:54 +0100

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+export function parse(sql: string): { query: string; comments: Record<string, string> } {
+	const trimmedSql = sql.trim().replace(/;\s*$/, '');
+	const start = trimmedSql.lastIndexOf('/*');
+	const end = trimmedSql.lastIndexOf('*/');
+
+	if (start > end || end !== trimmedSql.length - 2) return { query: sql, comments: {} };
+
+	const query = trimmedSql.substring(0, start);
+	const rawComment = trimmedSql.substring(start + 2, end);
+
+	const comments = Object.fromEntries(
+		rawComment.split(',').map((kv) => {
+			const [rawKey, rawValue] = kv.split('=');
+			const key = decodeURIComponent(rawKey);
+			const value = decodeURIComponent(rawValue.slice(1, -1));
+			return [key, value];
+		})
+	);
+
+	return { query, comments };
+}
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,30 @@
-import { myPackage } from '../src';
+import { parse } from '../src';
 
-describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
+describe('sql-comment-parse', () => {
+	describe('parser', () => {
+		it('should parse sqlcommenter sql', () => {
+			const sql =
+				"SELECT * FROM FOO /*action='%2Fparam*d',controller='index',framework='spring',traceparent='00-5bd66ef5095369c7b0d1f8f4bd33716a-c532cb4098ac3dd2-01',tracestate='congo%3Dt61rcWkgMzE%2Crojo%3D00f067aa0ba902b7'*/";
 
-      const result = myPackage(message);
+			const { query, comments } = parse(sql);
 
-      expect(result).toMatch(message);
-    });
-  });
+			expect(query).toMatch('SELECT * FROM FOO');
+			expect(comments).toEqual({
+				action: '/param*d',
+				controller: 'index',
+				framework: 'spring',
+				traceparent: '00-5bd66ef5095369c7b0d1f8f4bd33716a-c532cb4098ac3dd2-01',
+				tracestate: 'congo=t61rcWkgMzE,rojo=00f067aa0ba902b7',
+			});
+		});
+
+		it('should return empty on no comment', () => {
+			const sql = 'SELECT * FROM FOO';
+
+			const { query, comments } = parse(sql);
+
+			expect(query).toMatch('SELECT * FROM FOO');
+			expect(comments).toEqual({});
+		});
+	});
 });
